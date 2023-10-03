f83502dec1454c14908fe72ad46eda71ce128e5f
aoirint <aoirint@gmail.com>
2023-10-03 19:41:19 +0900

    pysen run format

--- lastfm_api_middleware/lastfm_api.py
+++ lastfm_api_middleware/lastfm_api.py
@@ -1,28 +1,29 @@
 from pathlib import Path
+
 import requests
 
 
 def dump_recenttracks(
-  lastfm_user: str,
-  lastfm_api_key: str,
-  useragent: str,
-  dump_path: Path,
+    lastfm_user: str,
+    lastfm_api_key: str,
+    useragent: str,
+    dump_path: Path,
 ):
-  api_url = 'https://ws.audioscrobbler.com/2.0/'
+    api_url = "https://ws.audioscrobbler.com/2.0/"
 
-  params = {
-    'method': 'user.getrecenttracks',
-    'user': lastfm_user,
-    'api_key': lastfm_api_key,
-    'format': 'json',
-  }
+    params = {
+        "method": "user.getrecenttracks",
+        "user": lastfm_user,
+        "api_key": lastfm_api_key,
+        "format": "json",
+    }
 
-  headers = {
-    'User-Agent': useragent,
-  }
+    headers = {
+        "User-Agent": useragent,
+    }
 
-  res = requests.get(api_url, params=params, headers=headers)
-  res.raise_for_status()
+    res = requests.get(api_url, params=params, headers=headers)
+    res.raise_for_status()
 
-  dump_path.parent.mkdir(parents=True, exist_ok=True)
-  dump_path.write_text(res.text, encoding='utf-8')
+    dump_path.parent.mkdir(parents=True, exist_ok=True)
+    dump_path.write_text(res.text, encoding="utf-8")
--- main.py
+++ main.py
@@ -1,49 +1,57 @@
 import os
+from datetime import datetime, timedelta, timezone
 from pathlib import Path
 from typing import Optional
-from datetime import datetime, timedelta, timezone
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
-
 from lastfm_api_middleware import lastfm_api
 
+LASTFM_USER = os.environ["LASTFM_USER"]
+LASTFM_API_KEY = os.environ["LASTFM_API_KEY"]
+DUMP_PATH = Path(os.environ["DUMP_PATH"])
+CORS_ALLOW_ORIGINS = os.environ["CORS_ALLOW_ORIGINS"].split(",")
 
-LASTFM_USER = os.environ['LASTFM_USER']
-LASTFM_API_KEY = os.environ['LASTFM_API_KEY']
-DUMP_PATH = Path(os.environ['DUMP_PATH'])
-CORS_ALLOW_ORIGINS = os.environ['CORS_ALLOW_ORIGINS'].split(',')
-
-USERAGENT = 'aoirint_lastfm_api_middleware/0.0.0'
+USERAGENT = "aoirint_lastfm_api_middleware/0.0.0"
 
 app = FastAPI()
 app.add_middleware(
-  CORSMiddleware,
-  allow_origins=CORS_ALLOW_ORIGINS,
-  allow_credentials=True,
-  allow_methods=['*'],
-  allow_headers=['*'],
+    CORSMiddleware,
+    allow_origins=CORS_ALLOW_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 recenttracks_last_fetched: Optional[datetime] = None
 recenttracks_interval = timedelta(seconds=60)
 
 
-@app.get('/v1/recenttracks')
+@app.get("/v1/recenttracks")
 def v1_recenttracks():
-  global recenttracks_last_fetched
+    global recenttracks_last_fetched
 
-  now = datetime.now(tz=timezone.utc)
-  if recenttracks_last_fetched is None or recenttracks_interval <= now - recenttracks_last_fetched:
-    recenttracks_last_fetched_string = recenttracks_last_fetched.isoformat() if recenttracks_last_fetched is not None else 'None'
-    print(f'[{now.isoformat()}] Fetch recenttracks (last_fetched_at: {recenttracks_last_fetched_string})')
+    now = datetime.now(tz=timezone.utc)
+    if (
+        recenttracks_last_fetched is None
+        or recenttracks_interval <= now - recenttracks_last_fetched
+    ):
+        recenttracks_last_fetched_string = (
+            recenttracks_last_fetched.isoformat()
+            if recenttracks_last_fetched is not None
+            else "None"
+        )
+        print(
+            f"[{now.isoformat()}] Fetch recenttracks (last_fetched_at: {recenttracks_last_fetched_string})"
+        )
 
-    lastfm_api.dump_recenttracks(
-      lastfm_user=LASTFM_USER,
-      lastfm_api_key=LASTFM_API_KEY,
-      useragent=USERAGENT,
-      dump_path=DUMP_PATH,
-    )
-    recenttracks_last_fetched = now
+        lastfm_api.dump_recenttracks(
+            lastfm_user=LASTFM_USER,
+            lastfm_api_key=LASTFM_API_KEY,
+            useragent=USERAGENT,
+            dump_path=DUMP_PATH,
+        )
+        recenttracks_last_fetched = now
 
-  return FileResponse(DUMP_PATH)
+    return FileResponse(DUMP_PATH)
